e959812916c041130415b6be67b2df490d321b29
Ambroise07 <ambroise.houndonougbo@gmail.com>
2019-06-15 03:52:12 +0100

    Les opérateurs en python

---
+++ 1-syntaxe/syntaxe.py
@@ -0,0 +1,11 @@
+#-*-coding:utf-8-*-
+#coding:utf-8
+age = 15 
+if age < 18 :
+    print(age)
+#ceci est un commentaire concercnant mon code
+"""
+simple commentaire -
+"""
+'''
+'''
---
+++ 2-variable/variable.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+
+longueur = 45
+largeur = 15
+resul = longueur - largeur
+print("Le résultat est : ", resul)
+L = 554
+Lalongeur_de_mon_triangle = 45
+Age = 12
+AGE = 12
+
+#Il est à noter que les deux variables age sont deux variables différentes
+"""
+Mème si elles contiennent des valeurs identiques elles ne sont pas les mèmes 
+j'ai oublié de montrer l'exemple dans la vidéo donc n'exiter par à vous entrainez
+pour voir la différence ! age est différent de AGE 
+"""
