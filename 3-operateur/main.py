c50fedf280225fc10a1e54c17a52505b438d20fb
Ambroise07 <ambroise.houndonougbo@gmail.com>
2019-06-15 05:02:30 +0100

    notion opérateur finish

--- 3-operateur/main.py
+++ 3-operateur/main.py
@@ -1,1 +1,31 @@
-#coding utf
+#coding:utf-8
+
+ballon = 12
+chapeau = 255
+achat = ballon + chapeau
+achat = achat ** 2
+print("le prix total est : ",achat)
+
+x = 55
+y = 5 
+T = 55 / 5
+N = 55 // 5 
+print("La valeur numero 1 {} La valeur numero 2 {}".format(type(T), type(N)))
+
+print("Result 1 est : {} et Result 2 est : {} ".format(T, N))
+
+print("Bienvenue sur notre programme d'achat")
+
+answer_chap = input("Entrer le prix de chapeau ")
+answer_bal = input("Entrer le prix de votre ballon ")
+
+Total = int(answer_chap) + int(answer_bal)
+
+print("Le prix total de vos achats sont : {} $".format(Total))
+
+modulo = Total % y
+
+
+print(modulo)
+
+
