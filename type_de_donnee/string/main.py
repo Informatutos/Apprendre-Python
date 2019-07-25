440c356bdf65521b2abb90d3e457302302f4c9bf
Ambroise07 <ambroise.houndonougbo@gmail.com>
2019-07-26 00:22:45 +0100

    Notion de type de donnée et chaine de charactère

---
+++ type_de_donnee/string/main.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+"""chaine = 'j\'aime mon pc '
+chaine1 = '"le language python"'"""
+
+notreChaine = "je suis un programmeur en python"
+"""chaine_count = notreChaine.count('e')"""
+"""chaine_len = len(notreChaine)"""
+"""chaine_upper = notreChaine.upper()"""
+"""chaine_lower = notreChaine.lower()"""
+"""chaine_split = notreChaine.split()"""
+"""chaine_index = notreChaine.index('p')"""
+"""chaine_a = notreChaine[1:3]"""
+"""if notreChaine.islower():
+    print('c\'est en minuscule')
+else :
+    print("c'est en majuscule ")"""
+print(notreChaine)
+notr = notreChaine.replace('p', 'V')
+print(notr)
+
