da6b9ac24229f1a7618ef7e72a240819bd8334fe
Ambroise07 <ambroise.houndonougbo@gmail.com>
2019-08-15 13:03:11 +0100

    Notion de tuples en Python

---
+++ type_de_donnee/tuples/main.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+var_tuples = ('4',45,'paris','lagos','rome',45.25)
+
+"""for i in range(len(var_tuples)):
+    print("le numero : {} ==> ".format(i), var_tuples[i])"""
+
+"""variable1,variable2,variable3 = 'toto','Vpn','Host'
+print(variable1)
+print(variable2)
+print(variable3)"""
+
+print(var_tuples.index('lagos'))
