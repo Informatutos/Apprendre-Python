47b951205338222b291fd96520e51ca9cc3afcf8
Ambroise07 <ambroise.houndonougbo@gmail.com>
2019-09-03 23:21:15 +0100

    Notion de Dictionnaire en Python

---
+++ type_de_donnee/dictionnaire/main.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+dic = {'Facebook':'18.000 users', 'Youtube':'24.000 users', 'Twitter':'65.0004 users', 'Instagram':'47.000 users'}
+"""dic['Facebook'] = '18.000 users'
+dic['Youtube'] = '24.000 users'
+dic['Twitter'] = '65.0004 users'
+dic['Instagram'] = '47.000 users'"""
+
+dic1 = {"Linux":789, (2,5):'Windows', 79:'MacOs'}
+
+print(dic.get('FacebooH', 'Facebook users'))
+
+
