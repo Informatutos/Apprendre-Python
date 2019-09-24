3aed8c91f37bb3decc8b20d16a0c13e63435915a
Ambroise07 <ambroise.houndonougbo@gmail.com>
2019-09-24 11:34:08 +0100

    les types de données , la notion de set en python

---
+++ type_de_donnee/set/main.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+#ensemb = set(['ballon',78,(78,96)])
+ensemb = set()
+#ensemb.add("facebook")
+ensemb.update(['ballon',78,(78,96)])
+#ensemb.remove('ballon1')
+#ensemb.discard('ballon1')
+#ensemb.clear()
+#ensemb.pop()
+#em = ensemb.copy()
+em = set([78,5, 'google'])
+#result = ensemb.intersection(em)
+#result = ensemb.union(em)
+#em.issubset(ensemb)
+#em.isuperset(ensemb)
+"""for i in ensemb:
+    print(i)
+
+if 'ballon' in ensemb :
+    print("ok")"""
