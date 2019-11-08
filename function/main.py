e65211ffd00333e357fcb64a8f9f0d4f3d2590bc
Ambroise07 <ambroise.houndonougbo@gmail.com>
2019-11-08 11:05:16 +0100

    La notion des fonctions en python

--- function/main.py
+++ function/main.py
@@ -1,1 +1,14 @@
-#coding:utf-8
+#coding:utf-8 
+def maSomme() :
+    x = 50
+    y = 25
+    print("La somme est : " , x + y)
+
+
+def maSomme2() :
+    maSomme()
+    print("je suis la deuxiemme somme ")
+
+
+multip = lambda x : x * 2
+print(multip(6))
