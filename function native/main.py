6a90e63dd1f26ed74d16f8e32b8d568be5355d97
Ambroise07 <ambroise.houndonougbo@gmail.com>
2020-01-01 04:41:42 +0100

    Les fonctions natives en Python

---
+++ function native/main.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+
+def Newyear(name: str, year : int) -> list:
+    if(type(name) != str):
+        exit("je veux une chaine de caractère")
+    print(name , "we are programmers !")
+
+print(min([25, 52, 100]))
+
