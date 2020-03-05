1c0e620a57831f0e928cce72280d80a1e9c4a40b
Ambroise07 <ambroise.houndonougbo@gmail.com>
2020-03-05 10:09:52 +0100

    la notion des modules en python

---
+++ modules/doc/modulePy.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+from math import sqrt
+
+def vitesse() :
+    print('Je roule à 25km/h')
+
+def calculeX(x) :
+
+    return sqrt(x) + x * 5
+
+
+
---
+++ modules/main.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+#import math
+#from math import sqrt, cos, sin
+#import doc.modulePy as Dm
+from os import system as CMD
+CMD('clear')
+from doc.modulePy import calculeX as X
+#Dm.vitesse()
+x = int(input('Entrer un nombre '))
+print('Le résultat est : ', X(x))
